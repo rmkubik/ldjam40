760be7b9950fb8b01348ed23dae8e7fd24caf9a1
rmkubik <rmkubik@me.com>
2017-12-03 14:54:11 -0800

    Add consumer component

---
+++ src/components/Consumer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+
+import DesktopIcon from './DesktopIcon';
+
+class Consumer extends DesktopIcon { 
+    componentDidMount() {
+        setInterval(() => { this.props.consumeCallback(this.props.consumedIcon, this.props.initialPosition, this.props.range) }, 1000);
+    }
+}
+
+export default Consumer;
