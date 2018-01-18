7b6cf9af0cca7f44cc969767a4285841c91c2809
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-01-17 22:24:06 -0800

    Remove comments

--- src/components/Consumer.js
+++ src/components/Consumer.js
@@ -3,17 +3,14 @@ import Draggable from 'react-draggable';
 
 import DesktopIcon from './DesktopIcon';
 
-class Consumer extends React.Component { 
+class Consumer extends React.Component {
     componentDidMount() {
         setInterval(() => { 
-            // do the callback
-            // set the state to be exiting
-            // once animation is done call remove from state
             this.props.consumeCallback(
-                this.props.consumedIcon, 
-                this.props.findPosition(), 
+                this.props.consumedIcon,
+                this.props.findPosition(),
                 this.props.range
-            ) 
+            )
         }, 1000);
     }
 
