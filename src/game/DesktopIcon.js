86f9b64fcc4ed7e56865a2abdff3fdd3b1b82d79
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-01-25 23:11:41 -0800

    Set initial values of DesktopIcon

--- src/game/DesktopIcon.js
+++ src/game/DesktopIcon.js
@@ -1,5 +1,20 @@
 class DesktopIcon {
+    
+    icon;
+    position;
+    initialPosition;
+    id;
+    destroyed;
+    consumed;
 
+    constructor(icon, initialPosition, id) {
+        this.icon = icon;
+        this.position = initialPosition;
+        this.initialPosition = initialPosition;
+        this.id = id;
+        this.destroyed = false;
+        this.consumed = 0;
+    }
 }
 
 export default DesktopIcon;
