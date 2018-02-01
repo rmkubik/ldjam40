4b72b34892963a891ba74dfc31817b67fcf38a19
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-01-31 20:31:12 -0800

    test that emit is called on all emitters

--- src/game/Game.test.js
+++ src/game/Game.test.js
@@ -1,15 +1,16 @@
+import DesktopIcon from './DesktopIcon';
+import Emitter from './Emitter';
 import Game from './Game';
 import GameState from './GameState';
-import DesktopIcon from './DesktopIcon';
 
 let game;
 
 beforeEach(() => {
     game = new Game();
-})
+});
 
 it('should have state object', () => {
-  expect(game.state).toEqual(expect.any(GameState));
+    expect(game.state).toEqual(expect.any(GameState));
 });
 
 it('should have icon types', () => {
@@ -21,5 +22,18 @@ describe('init game function', () => {
         game.init();
 
         expect(game.state.desktopIcons.length).toEqual(2);
+    });
+});
+
+describe('update function', () => {
+    it('should call emit on all desktopIcons in the state that have emit functions', () => {
+        game.init();
+
+        Emitter(game.state.desktopIcons[0], game.state, game.iconTypes.file);
+        const iconEmitSpy = jest.spyOn(game.state.desktopIcons[0], 'emit');
+
+        game.update();
+
+        expect(iconEmitSpy).toHaveBeenCalled();
     });
 });
