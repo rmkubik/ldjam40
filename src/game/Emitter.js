5763e6f8344c2c107bdc8338b45975c3df8f1db0
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-01-31 19:53:54 -0800

    create desktop icon in emit function

--- src/game/Emitter.js
+++ src/game/Emitter.js
@@ -1,6 +1,6 @@
-const Emitter = (desktopIcon, spawnedIcon) => {
+const Emitter = (desktopIcon, state, spawnedIcon) => {
     const emit = () => {
-
+        state.createDesktopIcon(desktopIcon.icon, desktopIcon.position);
     }
 
     Object.assign(
--- src/game/Emitter.test.js
+++ src/game/Emitter.test.js
@@ -12,7 +12,7 @@ beforeEach(() => {
 describe('Emitter functional mixin', () => {
     it('should add the emit function to an icon\'s prototype', () => {
         state.createDesktopIcon('a', {x: 0, y: 0});
-        Emitter(state.desktopIcons[0], 'a');
+        Emitter(state.desktopIcons[0], state, 'a');
         expect(state.desktopIcons[0].emit).toEqual(expect.anything());
     });
 });
@@ -20,7 +20,7 @@ describe('Emitter functional mixin', () => {
 describe('emit function', () => {
     it('should add a new desktopIcon to state of correct type', () => {
         state.createDesktopIcon('a', {x: 0, y: 0});
-        Emitter(state.desktopIcons[0], 'a');
+        Emitter(state.desktopIcons[0], state, 'a');
         state.desktopIcons[0].emit();
         expect(state.desktopIcons[1].icon).toEqual('a');
     });
