97705dee3c5ffca0f48d1c81e483956618a8b08c
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-02-06 18:44:00 -0800

    Test that emitter works off cooldown

--- src/game/Emitter.test.js
+++ src/game/Emitter.test.js
@@ -13,6 +13,7 @@ describe('Emitter functional mixin', () => {
     it('should add the emit function to an icon\'s prototype', () => {
         state.createDesktopIcon('a', {x: 0, y: 0});
         Emitter(state.desktopIcons[0], state, 'a');
+        
         expect(state.desktopIcons[0].emit).toEqual(expect.anything());
     });
 });
@@ -21,7 +22,9 @@ describe('emit function', () => {
     it('should add a new desktopIcon to state of correct type', () => {
         state.createDesktopIcon('a', {x: 0, y: 0});
         Emitter(state.desktopIcons[0], state, 'b');
+
         state.desktopIcons[0].emit();
+
         expect(state.desktopIcons[1].icon).toEqual('b');
     });
 
@@ -40,5 +43,22 @@ describe('emit function', () => {
         desktopIcon.emit();
 
         expect(state.desktopIcons.length).toEqual(1);
+    });
+
+    it('should emit when off cooldown', () => {
+        state.createDesktopIcon('a', {x: 0, y: 0});
+        const desktopIcon = state.desktopIcons[0];
+
+        Emitter(desktopIcon, state, 'b', 1000);
+        desktopIcon.lastEmitTimestamp = new Date('2018-01-01 12:00:00:0000').getTime();
+        Date.now = jest
+            .genMockFunction()
+            .mockReturnValue(
+                new Date(`2018-01-01 12:00:00:${desktopIcon.emitCooldown + 1}`).getTime()
+            );
+
+        desktopIcon.emit();
+
+        expect(state.desktopIcons.length).toEqual(2);
     });
 });
