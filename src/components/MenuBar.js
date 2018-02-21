ab3c7e20bb6197bc205c7fad585922dd19484e8e
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-02-21 00:00:50 -0800

    Dynamically add menu buttons in MenuBar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import PurchaseButton from './PurchaseButton';
+
 function MenuBar(props) {
     return (
-        <p className="menubar">{`$${props.money}`}</p>
+        <div>
+            <p className="menubar">{`$${props.money}`}</p>
+            {props.createButtons.map((createButton) => {
+                return (<PurchaseButton
+                    createDesktopIcon={props.createDesktopIcon}
+                    icon={createButton.icon}
+                    price={createButton.price}
+                />)
+            })}
+        </div>
     )
 }
 
