d6126cb3970ec6d050411683e55b6a81fbc5bff5
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-02-22 23:49:58 -0800

    Add HddBar into MenuBar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
 import PurchaseButton from './PurchaseButton';
+import HddBar from './HddBar';
 
 function MenuBar(props) {
     return (
         <div>
-            <p className="menubar">{`$${props.money}`}</p>
+            <p className="money">{`$${props.money}`}</p>
             {props.createButtons.map((createButton, index) => {
-                return (<PurchaseButton
-                    purchaseDesktopIcon={props.purchaseDesktopIcon}
-                    icon={createButton.icon}
-                    price={createButton.price}
-                    key={index}
-                />)
+                return (
+                    <PurchaseButton
+                        purchaseDesktopIcon={props.purchaseDesktopIcon}
+                        icon={createButton.icon}
+                        price={createButton.price}
+                        key={index}
+                    />
+                )
             })}
+            <HddBar
+                current={props.hdd.current}
+                max={props.hdd.max}
+            />
         </div>
     )
 }
